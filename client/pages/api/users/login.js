c02db0abba9e5a5d74dfc99cff984b15295c39ba
Andreas Krueger <a01649986@unet.univie.ac.at>
2022-12-19 20:41:07 +0100

    trying to fix login

--- client/pages/api/users/login.js
+++ client/pages/api/users/login.js
@@ -6,34 +6,43 @@ import client from "../../../utils/client";
 const handler = nc();
 
 handler.post(async (req, res) => {
-  const user = await client.fetch(`*[_type == "user" && email == $email][0]`, {
-    email: req.body.email,
-  });
+  try {
+    const user = await client.fetch(
+      `*[_type == "user" && email == $email][0]`,
+      {
+        email: req.body.email,
+      }
+    );
 
-  if (
-    user &&
-    bcrypt.compareSync(req.body.password, user.password) &&
-    !user.isBanned
-  ) {
-    const token = signToken({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-    });
-    res.send({
-      success: true,
-      message: `Welcome back ${user.name}`,
-      token,
-      name: user.name,
-    });
-    // redirect to home page
-    res.redirect("/");
-  } else {
-    res
-      .status(401)
-      .send({ success: false, message: "Invalid email or password" });
+    if (
+      user &&
+      bcrypt.compareSync(req.body.password, user.password) &&
+      !user.isBanned
+    ) {
+      const token = signToken({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      });
+      res.send({
+        success: true,
+        message: `Welcome back ${user.name}`,
+        token,
+        name: user.name,
+      });
+      // redirect to home page
+      res.redirect("/");
+    } else {
+      res
+        .status(401)
+        .send({ success: false, message: "Invalid email or password" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ success: false, message: error.message });
   }
+  // if status is 500, send error message
 });
 
 export default handler;
