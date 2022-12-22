b024055dbe238aae3cce5d82a6b8eec578a5860a
Andreas Krueger <a01649986@unet.univie.ac.at>
2022-12-22 10:29:37 +0100

    hopefully fixed

--- client/pages/race/[slug].js
+++ client/pages/race/[slug].js
@@ -8,21 +8,25 @@ const RaceView = () => {
   const { slug } = router.query;
   // get the race details from the api
   const [data, setData] = useState(null);
+  const getData = async () => {
+    const url = `https://yavqngwf.api.sanity.io/v1/data/query/production?query=*%5B_type%20%3D%3D%20%22race%22%20%26%26%20slug.current%20%3D%3D%20%24slug%20%5D&%24slug=%22${slug}%22`;
+    axios.get(url).then((res) => {
+      setData(res.data.result[0]);
+    });
+  };
 
   useEffect(() => {
     if (!data) {
-      const url = `https://yavqngwf.api.sanity.io/v1/data/query/production?query=*%5B_type%20%3D%3D%20%22race%22%20%26%26%20slug.current%20%3D%3D%20%24slug%20%5D&%24slug=%22${slug}%22`;
-      axios.get(url).then((res) => {
-        console.log("called");
-        setData(res.data.result[0]);
-      });
+      getData();
     }
   }, [data]);
 
   // const google_maps_url = `https://maps.google.com/maps?q=${raceData.startLocation.lat},${raceData.startLocation.lng}&t=&z=13&ie=UTF8&iwloc=&output=embed`;
 
   //add a "km to every distance in the array"
-
+  if (!data) {
+    getData();
+  }
   return data && <RacePage data={data} />;
 };
 export default RaceView;
